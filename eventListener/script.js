2959da3c90a55493a960b178789de9c690f5cf8f
IndranjanaChatterjee <112919974+IndranjanaChatterjee@users.noreply.github.com>
2023-05-08 12:42:12 +0530

    Add files via upload

---
+++ eventListener/script.js
@@ -0,0 +1,39 @@
+var a=document.body;
+        let x=function(e){
+            
+            a.style.background="red";
+            
+        }
+        let y=function(e){
+            
+            a.style.background="yellow";
+            
+        }
+        let z=function(e){
+            
+            a.style.background="blue";
+            
+        }
+        let aa=function(e){
+            
+            a.style.background="pink";
+            
+        }
+        let b=function(e){
+            
+            a.style.background="green";
+            
+        }
+        let c=function(e){
+            
+            a.style.background="cyan";
+            
+        }
+        
+        
+        btn.addEventListener('click',x);
+        btn1.addEventListener('click',y);
+        btn2.addEventListener('click',z);
+        btn3.addEventListener('click',aa);
+        btn4.addEventListener('click',b);
+        btn5.addEventListener('click',c);
